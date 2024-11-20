ca5d7e695fdc4ade7d3fee6708260daebb330936
Rodrigo <rodrixfloresm@gmail.com>
2024-11-20 16:39:49 -0500

    Subiendo estructura y evento

---
+++ app/src/main/java/com/example/dam_uc3_floresmayta/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.dam_uc3_floresmayta;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.idboton_reserva);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se reservó el vuelo",
+                Toast.LENGTH_SHORT).show());
+    }
+}
